d60d6126dcb2e3b61fc33d32d3ed95579c19b29e
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-08 08:53:31 -0700

    small caps

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+* Description: random number in n +ve/-ve
+* Return: 0 on success
+*/
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	return (0)
+}
