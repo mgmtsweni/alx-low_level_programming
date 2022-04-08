08a311cfa7e52c71f4a9a42641dc0a4c3ddff13f
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-08 08:31:07 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = % 10;
+	lastDigit = n % 10;
 	if (lastDigit > 5)
 		printf("Last digit %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	else if (lastDigit == 0)
