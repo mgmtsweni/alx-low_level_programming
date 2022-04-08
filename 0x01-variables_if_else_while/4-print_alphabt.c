8d46eb169900483ccc00f37359f89f3ca18834a5
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-08 09:45:07 -0700

    Alpha exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+* Description: program that prints the alphabet in lowercase
+* Return: 0 on success
+*/
+int main(void)
+{
+	char Alpha;
+	char capture;
+
+	Alpha = 'a';
+	while (Alpha <= 'z')
+	{
+		capture = Alpha;
+		Alpha++;
+	}
+	if (capture != 'q' && capture != 'e')
+		putchar(capture);
+	putchar('\n');
+	return (0);
+}
