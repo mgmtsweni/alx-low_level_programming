f45872195e40fb6071a1db655700382d7d1206b4
Gift Mtsweni <mtswenigift@gmail.com>
2022-04-12 18:15:05 +0200

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 /**
 * main - prints to string
 * Description: Prints positive, negative or zero
 * Return: 0
 */
-void	_putchar(char c)
+int	main(void)
 {
-		return(write(1, &c, 1));
+	char c[] = "_putchar";
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		_putchar(a[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
