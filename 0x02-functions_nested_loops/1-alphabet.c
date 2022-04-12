3e27eefba698556d3eabd53dd15f610cee8b905e
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-12 09:39:34 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* Description: prints the alphabets
+* print_alphabet- print alphabet in lowercase
 * Return: 0 on success
 */
 void print_alphabet(void)
 {
 	char a;
-	
+
 	a = 'a';
 	while (a <= 'z')
 	{
