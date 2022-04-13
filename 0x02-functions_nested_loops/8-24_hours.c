dd466896e91d08f96d0713e6fab2a2f6bbb6c064
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-12 18:41:41 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* jack_bauer - Entry point
+* Description: prints every minute of the day of Jack Bauer
+* Return: Always 0.
+*/
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+	
+	hr = 0;
+	while (hr < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		}
+		hr++;
+	}
+}
