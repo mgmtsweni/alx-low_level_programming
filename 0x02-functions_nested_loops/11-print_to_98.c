885a371b0ee9a6adacc2c44cf295625ae1818f7e
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-12 19:29:29 -0700

    Magic 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,20 @@
 #include <main.h>
+#include <stdio.h>
 /**
-* main - entry point
-* Description: prints the alphabets
+* print_to_98 - entry point
+* Description:  prints all natural numbers from n to 98
 * Return: 0 on success
+* @n: holds the character under evaluation101
 */
-int main(void)
+void print_to_98(int n)
 {
-	
-	return (0);
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+		printf("98\n");
 }
