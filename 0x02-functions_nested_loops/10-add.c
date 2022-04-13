9e7c1bae8c878d6cfb968227ea63585dcb2c1b71
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-12 19:04:50 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - Entry point
+* Description: sum of two intergers
+* Return: Always results.
+* @a: holds the character under evaluation101~
+* @b: holds the character under evaluation1
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
