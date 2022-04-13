7b2fbabbdd100a379af2a74b98e49bc4f505daf0
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-12 18:18:28 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_last_digit - Entry point
+* Description: print the last digit
+* Return: Always 0.
+* @x: holds the character under evaluation1
+*/
+int print_last_digit(int x)
+{
+	int i;
+	int num;
+
+	num = x % 10;
+	if (num < 0)
+	num = -num;
+	i = '0' + y;
+	_putchar(e);
+	return (y);
+}
