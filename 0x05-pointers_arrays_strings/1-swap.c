34995d3e688ad5fbc6e34a37f42086ec2706800c
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-21 04:50:27 -0700

    swap string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* swap_int - swap strings
+* @a: - input first pointer int
+* @b: - input second pointer int
+*/
+void swap_int(int *a, int *b)
+{
+	int *hold;
+
+	hold = a;
+	*a = *b;
+	*b = *hold;
+}
