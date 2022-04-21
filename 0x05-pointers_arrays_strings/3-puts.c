bb18eaf17b373dbc57eeb51227965e8b3ae05213
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-21 04:51:54 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
