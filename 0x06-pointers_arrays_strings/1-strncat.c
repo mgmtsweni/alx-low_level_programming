e8ecf684b01d742403c90177e05be03114db55be
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-22 04:13:32 -0700

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strncpy - concatenates two strings.
+* @src: input string
+* @dest: output string
+* @n: input byte
+* Return:  return
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int	i;
+	int	l;
+	
+	i = 0;
+	l = 0;
+	while (dest[l])
+		l++;
+	while (i < n && src[i])
+	{
+		dest[l] = src[i];
+		l++;
+		i++;
+	}
+	dest[l] = '\0';
+	return (dest);
+}
