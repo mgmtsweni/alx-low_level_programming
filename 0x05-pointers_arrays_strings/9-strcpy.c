9fcd0465a79123d8bf260b63956a465ff62d4df3
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-21 17:37:52 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,15 +12,5 @@ char *_strcpy(char *dest, char *src)
 	i = 0;
 	while (dest[i] = src[i])
 		i++;
-}
-
-int main(void)
-{
-	char s1[98];
-        char *ptr;
-
-	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
-	return (0);
+	return (dest);
 }
