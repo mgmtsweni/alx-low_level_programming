d524e671e321814cff55978e695dd774be6c6008
Mtsweni Gift <mtswenigift@gmail.com>
2022-04-25 16:07:18 -0700

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,15 @@
 #include "main.h"
+/**
+ * _memcpy - copies n bytes from memory area "src" to memory area "dest"
+ * @dest: the memory area destination.
+ * @src: the memory area source.
+ * @n: the number of bytes to copy from "src"
+ * Return: dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	size_t i;
-	
+
 	if (!dest && !src)
 		return (0);
 	i = 0;
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,8 +8,8 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != c)
-		if (!*s++)
-			return (0);
+	while (*s != '\0')
+		if (*s++ == c)
+			return (s);
 	return (s);
 }
