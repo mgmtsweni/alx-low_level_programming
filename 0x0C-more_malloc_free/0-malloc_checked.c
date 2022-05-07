edc13255253025f28b7e73ac9d7486afb676d742
Gift Mtsweni <mtswenigift@gmail.com>
2022-05-08 00:30:33 +0200

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,6 @@
 /**
   * malloc_checked - allocates memory using malloc.
   * @b: size of memory to be allocated.
-  *
   * Return: pointer to allocated memory.
   */
 void *malloc_checked(unsigned int b)
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,28 +1,36 @@
+#include <stdlib.h>
 #include "main.h"
 /**
- * array_range - creates an array of integers.
- * @min: minimum value.
- * @max: maximum value.
- * Return: pointer to the newly created array.
- * if man > mix, returns NULL.
- * if malloc fails, returns NULL.
- */
+  *array_range - creates an array of integers.
+  *@min: minimum value.
+  *@max: maximum value.
+  *
+  *Return: pointer to newly created array.
+  *NULL if malloc fails.
+  *NULL if min > max.
+  */
 int *array_range(int min, int max)
 {
+	int range, i;
 	int *p;
-	int i;
+
+	range = 0;
 
 	if (min > max)
 		return (NULL);
 
-	p = malloc(sizeof(*p) * ((max - min) + 1));
+	range = ((max + 1) - min);
+
+	p = malloc(range * sizeof(int));
 
 	if (p == NULL)
 		return (NULL);
 
-	i = 0;
-	while (min <= max)
-		p[i++] = min++;
+	for (i = 0; i < range; i++)
+	{
+		*(p + i) = min + i;
+	}
 
 	return (p);
 }
+
