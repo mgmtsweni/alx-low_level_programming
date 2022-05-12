15054c7814f6c4d761bf06f4cb5c7fa4c0cbbf15
Gift Mtsweni <mtswenigift@gmail.com>
2022-05-12 05:04:17 +0200

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,9 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp || size <= 0)
-			return (-1);
-
+	if (!array || size <= 0 || !cmp)
+		return (-1);
 	i = 0;
 	while (i < size)
 	{
@@ -23,6 +22,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (i);
 		i++;
 	}
-	
 	return (-1);
 }
