d48d663c6e2ba8b8d0e8f5e27380965ce824294d
Gift Mtsweni <mtswenigift@gmail.com>
2022-05-26 18:35:14 +0200

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,17 +6,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int value = 0;
-    int i;
+	unsigned int value = 0;
 
-    if (!b)
-        return (0);
-    
-    while (b[i] == '0' || b[i] == '1')
-    {
-        value <<= 1;
-        value += b[i] - '0';
-        i++;
-    }
-     return (value);
+	if (!b)
+		return (0);
+
+	while (*b)
+	{
+		if (*b < '0' || *b > '1')
+			return (0);
+		value <<= 1;
+		value += *b++ - '0';
+	}
+	return (value);
 }
