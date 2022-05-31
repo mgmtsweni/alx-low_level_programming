2472abd421210333327fca6bf27a57da1d4b233f
Gift Mtsweni <mtswenigift@gmail.com>
2022-05-31 22:50:39 +0200

    File O/I

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,36 @@
-#include "main.h"
+/*
+ * Auth: Brennan D Baraban
+ * File: 0-binary_to_uint.c
+ */
+
+#include "holberton.h"
+
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
+ *
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  *         Otherwise - the converted number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0, mult = 1;
-	int l;
+	unsigned int num = 0, mult = 1;
+	int len;
 
 	if (b == '\0')
 		return (0);
 
-	for (l = 0; b[l];)
-		l++;
+	for (len = 0; b[len];)
+		len++;
 
-	for (l -= 1; l >= 0; l--)
+	for (len -= 1; len >= 0; len--)
 	{
-		if (b[l] != '0' && b[l] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
-		value += (b[l] - '0') * mult;
+		num += (b[len] - '0') * mult;
 		mult *= 2;
 	}
-	return (value);
+
+	return (num);
 }
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,6 @@
+file_io
+
+Task 0: Write a function that read a text file and prints it to the POSIX standard output.
+Task 1: Create a function that creates a file
+Task 2: Write a function that appends text at the end of a file 
+Task 3: Write a program that copies the content of a file to another file
