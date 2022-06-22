eeed58530d40fdc593e6b1bdfd8523652ce6cdd4
Gift Mtsweni <mtswenigift@gmail.com>
2022-06-22 13:50:11 +0200

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h> /* printf */
-#include <stdlib.h> /* malloc, free */
-#include <string.h> /* size_t */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  * for Holberton project
  */
@@ -33,5 +33,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
 #endif
