a37200c66637ca64406200ba316892b7d45d9b07
Gift Mtsweni <mtswenigift@gmail.com>
2022-07-12 02:13:14 +0200

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/functions/_strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locate 1st occurrence of char in string and returns pointer there
+ * @s: string to search
+ * @c: target characer
+ * Return: pointer to that character in string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char*)s);
+		++s;
+	}
+	if (c == '\0')
+		return ((char*)s);
+	return (0);
+}
