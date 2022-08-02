e6fcdc2ec45b4481167393d0647ac0bddc2ca6fc
mgmtsweni <mtswenigift@gmail.com>
2022-08-02 22:22:42 +0200

    update

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key of the value to retrieve
+ * Return: value associated with element || NULL if not be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node = NULL;
+
+	if (!ht || !key || !strcmp(key, ""))
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = (ht->array)[index];
+
+	while (node)
+	{
+		if (!strcmp(node->key, (char *)key))
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+ C - Hash tables
+
+Hash Table is a data structure which stores data in an associative manner. In hash table, the data is stored in an array format where each data value has its own unique index value. Access of data becomes very fast, if we know the index of the desired data.
