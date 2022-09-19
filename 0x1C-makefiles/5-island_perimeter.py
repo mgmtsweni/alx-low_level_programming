2e79acc8180361acdbe0c7824e29fe4a1c61ac59
mgmtsweni <mtswenigift@gmail.com>
2022-09-19 15:40:25 +0200

    island parameter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,11 +8,11 @@ def island_perimeter(grid):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
                 if row == 0 or grid[row - 1][col] == 0:
-                    p += 1  # top
+                    p += 1
                 if row == (len(grid) - 1) or grid[row + 1][col] == 0:
-                    p += 1  # bottom
+                    p += 1
                 if col == 0 or grid[row][col - 1] == 0:
-                    p += 1  # left
+                    p += 1
                 if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
-                    p += 1  # right
+                    p += 1
     return p
