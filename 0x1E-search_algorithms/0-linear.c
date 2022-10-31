8d0d6fc01decea43b4aa9de1c3b34336c2af228e
mgmtsweni <mtswenigift@gmail.com>
2022-10-31 18:55:59 +0200

    Linear Search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/*
+* liner_search - searches for a int value in a
+* @array - a pointer to the first element
+* @size - size of the array
+* @value - value to be search
+* return -1 if not found || value if it's found
+*/
+int linear_search(int *array, size_t size, int value)
+{
+    int i;
+
+    if (array == NULL)
+        return (-1);
+    i = 0;
+    while (i < (int)size)
+    {
+            printf("Value checked array[%u] = [%d]\n", i, array[i]);
+            if (array[i] == value)
+                return (value);
+            i++;
+    }
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+/*
+* liner_search - searches for a int value in a
+* @array - a pointer to the first element
+* @size - size of the array
+* @value - value to be search
+* return -1 if not found || value if it's found
+*/
+int binary_search(int *array, size_t size, int value);
+{
+    int i;
+
+    if (array == NULL)
+        return (-1);
+    i = 0,
+    
+    return (-1);
+}
