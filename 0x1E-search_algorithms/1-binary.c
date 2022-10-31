7f9ae4bfe235f842b10d9c48e99abb5effb73698
mgmtsweni <mtswenigift@gmail.com>
2022-11-01 01:07:34 +0200

    Binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,15 +4,29 @@
 * @array - a pointer to the first element
 * @size - size of the array
 * @value - value to be search
-* return -1 if not found || value if it's found
+* return (-1) if not found || (value) if it's found
 */
-int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value)
 {
-    int i;
+    size_t i, end, start;
 
     if (array == NULL)
         return (-1);
-    i = 0,
-    
+
+    for (start = 0, end = size - 1; end >= start;)
+    {
+        printf("Searching in array: ");
+        for (i = start; i < end; i++)
+            printf("%d, ", array[i]);
+        printf("%d\n", array[i]);
+
+        i = start + (end - start) / 2;
+        if (array[i] == value)
+            return (value);
+        else if (array[i] > value)
+            end = i - 1;
+        else
+            start = i + 1;    
+    }
     return (-1);
 }
