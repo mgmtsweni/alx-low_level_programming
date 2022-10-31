51678f6b95fc31c3e3e941b8edd7d3489e8a3f13
mgmtsweni <mtswenigift@gmail.com>
2022-11-01 01:40:43 +0200

    update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,15 +8,15 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-    int i;
+    size_t i;
 
     if (array == NULL)
         return (-1);
-    for (i = 0; i < (int)size; i++)
+    for (i = 0; i < size; i++)
     {
             printf("Value checked array[%u] = [%d]\n", i, array[i]);
             if (array[i] == value)
-                return (value);
+                return (i);
     }
     return (-1);
 }
